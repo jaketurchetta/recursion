3defb0cbc15b60b8145b37ba7c9ed32896d0d5d9
Jake Turchetta <14jturchetta@gmail.com>
2020-03-16 19:19:44 -0700

    Commit final recursion changes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,30 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var result = [];
+
+  function findElements(result, className, element){
+  	// identify starting node
+  	var element = element || document.body;
+
+  	// case 1: element with classes
+  	if(element.classList && element.classList.contains(className)){
+  		result.push(element);
+  	}
+  	
+
+  	// case 2: element with child nodes
+  	if(element.childNodes){
+  		_.each(element.childNodes, function(item){
+  				findElements(result, className, item);
+  		})
+  		
+  		return result;
+  	}
+  }
+
+  findElements(result, className, document.body);
+
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // define result variable
+  var finalString = '';
+
+  // case 1: undefined and function inputs
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+  	return '';
+  }
+
+  // case 2: obj is null
+  if(obj === null) {
+  	return 'null';
+  }
+
+  // case 3: obj is number or boolean
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+  	finalString += obj;
+  }
+
+  // case 4: obj is a string
+  if (typeof obj === 'string') {
+  	obj = '\"' + obj + '\"';
+  	finalString += obj;
+  }
+
+  // case 5: empty array
+  if (Array.isArray(obj) && obj.length < 1) {
+  	return finalString += '[]';
+  }
+
+  // case 6: populated array
+  if(Array.isArray(obj) === true){
+  		var finalArray = [];
+  		_.each(obj, function(item){
+  			finalArray.push(stringifyJSON(item));
+  		})
+
+  		return finalString += '[' + finalArray + ']';
+  	}
+
+  // case 7: object
+  if (typeof obj === 'object') {
+  	for (var key in obj) {
+  		if(typeof obj[key] !== 'function' && obj[key] !== undefined) {
+  			finalString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+  		}
+  	}
+  	return '{' + finalString.slice(0,finalString.length-1) + '}';
+  }
+
+  return finalString;
+
 };
